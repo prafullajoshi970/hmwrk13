383d0400acf7f4af66483e0a6db3ae3d01dfca2b
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2023-04-28 21:22:27 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Products from "./component/product";
+
+function App() {
+  return (
+    <div className="App">
+      <Products></Products>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/product.js
@@ -0,0 +1,49 @@
+
+
+import { useEffect,useState } from "react";
+
+function Products(props) {
+  const[data,setData]=useState([]);
+
+useEffect(() => {
+
+    fetch("https://fakestoreapi.com/products")
+    .then (resp =>resp.json())
+    .then(data=>setData(data))
+    
+    .catch(error=>console.log(error))
+
+
+},[])
+
+console.log(data)
+
+
+  return (
+    <div className='NavBarc'>
+     
+    {
+      data.length >0 ?(
+         <div className='cardContainer'>
+            {
+               data.map((product)=>{
+                return( <div className='card'>
+                  <img src={product.image} alt=""></img>
+                  <h6>{product.title}</h6>
+                  <p>{product.category}</p>
+                  <p>{product.rating.rate +'⭐'}</p>
+                
+                  </div>) 
+               })
+
+            }
+         </div>
+      ):(<h2>NoData Found</h2>)
+    }
+     
+     
+    </div>
+  )
+}
+
+export default Products;
